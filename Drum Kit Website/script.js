42825d5506b8b2f7f2622152ba4e8927e9b25da1
Arymus <arymusr@gmail.com>
2024-11-08 21:10:45 -0500

    Add files via upload

---
+++ Drum Kit Website/script.js
@@ -0,0 +1,35 @@
+const kick = document.getElementById("kick"); // F key
+const snare = document.getElementById("snare"); // K key
+const crash = document.getElementById("crash"); // V key
+const ride = document.getElementById("ride"); // T key
+const tom = document.getElementById("tom"); // G key
+
+const kickAudio = document.getElementById("kickAudio");
+const snareAudio = document.getElementById("snareAudio");
+const crashAudio = document.getElementById("crashAudio");
+const rideAudio = document.getElementById("rideAudio");
+const tomAudio = document.getElementById("tomAudio");
+const hiHatAudio = document.getElementById("hiHat");
+
+const drumKit = [
+    {key:"f", button: kick, audio: kickAudio},
+    {key: "k", button: snare, audio: snareAudio},
+    {key: "v", button: crash, audio: crashAudio},
+    {key: "t", button: ride, audio: rideAudio},
+    {key: "g", button: tom, audio: tomAudio},
+    {key: "h", button: hiHat, audio: hiHatAudio}
+];
+
+function playSound(audio) {
+    audio.currentTime = 0;
+    audio.play();
+};
+
+document.addEventListener("keydown", function(event) {
+    const key = event.key.toLowerCase();
+    const drum = drumKit.find(drum => drum.key === key);
+
+    if (drum) {
+        playSound(drum.audio); 
+    }
+});
